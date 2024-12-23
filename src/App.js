b6f36ea0cd3487c0bde710cd9b1132c46ba89efc
Pelham Barron <pelham@umich.edu>
2024-12-23 18:52:38 -0500

    Initial commit: Set up React app with Yarn

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Fantasy Football Eliminator!</h1>
+      {/* Add more components or content here */}
+    </div>
+  );
+}
+
+export default App;
